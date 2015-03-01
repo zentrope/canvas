02e25935b314e92b7eaf7c0acb83bdd27ffc4a14
Keith Irwin <keith@zentrope.com>
2015-03-01 15:05:04 -0800

    Drawing scales to window size without bluring.

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # canvas
 
 Playing around with canvas.
+
+
+## Some goals
+
+- How do you allow the canvas to scale with window size?
+- How do you allow the contents of the canvas to scale and not blur?
+- Does ClojureScript make working with this stuff easier?
+- How do you represent objects?
+- How do you animate?
+- How do you best use requestAnimation?
+- How "functional" can you make all this?
--- src/canvas/main.cljs
+++ src/canvas/main.cljs
@@ -37,38 +37,48 @@
     ctx))
 
 (def ctx (.getContext (by-id "canvas") "2d"))
+(def scale-w 800)
+(def scale-h 450)
 
 (defn draw!
   [ctx]
-  (let [c (by-id "canvas")
-        w (.-width c)
-        h (.-height c)]
-   (-> ctx
-       (ctx-prop! "fillStyle" "rgb(100, 100, 100)")
-       (ctx-prop! "strokeStyle" "black")
-       (ctx-apply "fillRect" 0 0 w h)
-       (ctx-prop! "fillStyle" "rgb(200, 0, 0)")
-       (ctx-apply "strokeRect" 10 10 55 50)
-       (ctx-apply "fillRect" 10 10 55 50)
-       (ctx-prop! "fillStyle" "rgba(0,0,200,0.5)")
-       (ctx-apply "strokeRect" 30 30 55 50)
-       (ctx-apply "fillRect" 30 30 55 50))))
+  (let [c (by-id "canvas")]
+    (-> ctx
+        (ctx-apply "clearRect" 0 0 scale-w scale-h)
+        (ctx-prop! "fillStyle" "green")
+        (ctx-apply "fillRect" 0 0 scale-w scale-h)
+        (ctx-prop! "lineWidth" "0.5")
+        (ctx-prop! "strokeStyle" "black")
+        (ctx-prop! "fillStyle" "rgb(200, 0, 0)")
+        (ctx-apply "strokeRect" 10 10 110 100)
+        (ctx-apply "fillRect" 10 10 110 100)
+        (ctx-prop! "fillStyle" "rgba(0,0,200,0.5)")
+        (ctx-apply "strokeRect" 60 60 110 100)
+        (ctx-apply "fillRect" 60 60 110 100)
+        ;;
+        ;; See if the virtual bounds are what I think they are.
+        (ctx-prop! "lineWidth" "4")
+        (ctx-prop! "strokeStyle" "dodgerblue")
+        (ctx-apply "strokeRect" 0 0 scale-w scale-h))))
 
 (defn resize!
   [e]
+  ;; Maintains 16x9
   (let [w (- (.-innerWidth js/window) 40)
-        h (- (.-innerHeight js/window) 40)]
+        h (- (int (/ (* w 9) 16)) 40)]
     (-> (by-id "canvas")
-        (style! "width" (str w "px"))
-        (style! "height" (str h "px"))
+        ;; (style! "width" (str w "px"))
+        ;; (style! "height" (str h "px"))
         (attr! "width" w)
         (attr! "height" h))
+    (.scale ctx (/ w scale-w) (/ h scale-h))
     (draw! ctx)))
 
 (defn- main
   []
   (println "Welcome to the Canvas Scratch App.")
   (listen! js/window "resize" resize!)
+  (draw! ctx)
   (resize! nil)
   (draw! ctx))
 
