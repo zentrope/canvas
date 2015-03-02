5a0f00b5ea82ffec73076618dfe7519ffcd9ba72
Keith Irwin <keith@zentrope.com>
2015-03-01 17:51:43 -0800

    Protocols!

--- src/canvas/main.cljs
+++ src/canvas/main.cljs
@@ -2,6 +2,10 @@
 
 (enable-console-print!)
 
+;;-----------------------------------------------------------------------------
+;; DOM
+;;-----------------------------------------------------------------------------
+
 (defn by-id
   [id]
   (.getElementById js/document id))
@@ -25,6 +29,10 @@
   [el type fn]
   (.addEventListener el type fn false))
 
+;;-----------------------------------------------------------------------------
+;; Canvas Context
+;;-----------------------------------------------------------------------------
+
 (defn ctx-prop!
   [ctx attr value]
   (aset ctx attr value)
@@ -36,30 +44,68 @@
     (.apply f ctx (apply array args))
     ctx))
 
+;;-----------------------------------------------------------------------------
+
+(defrecord Rect [x y width height color])
+(defrecord Frame [x y width height color frame-width])
+(defrecord Paddle [x y width height color])
+
+;;-----------------------------------------------------------------------------
+
+(defprotocol IDrawable
+  (draw! [_ ctx]))
+
+(extend-type Paddle
+  IDrawable
+  (draw! [paddle ctx]
+    (let [{:keys [color x y width height]} paddle]
+      (aset ctx "fillStyle" color)
+      (.fillRect ctx x y width height))
+    ctx))
+
+(extend-type Rect
+  IDrawable
+  (draw! [rect ctx]
+    (let [{:keys [color x y width height]} rect]
+      (aset ctx "lineWidth" "0.5")
+      (aset ctx "fillStyle" color)
+      (aset ctx "strokeStyle" "black")
+      (.fillRect ctx x y width height)
+      (.strokeRect ctx x y width height))
+    ctx))
+
+(extend-type Frame
+  IDrawable
+  (draw! [frame ctx]
+    (let [{:keys [color x y width height frame-width]} frame]
+      (aset ctx "lineWidth" (str frame-width))
+      (aset ctx "strokeStyle" color)
+      (.strokeRect ctx x y width height))
+    ctx))
+
+;;-----------------------------------------------------------------------------
+
 (def ctx (.getContext (by-id "canvas") "2d"))
 (def scale-w 800)
 (def scale-h 450)
 
-(defn draw!
+(def objects
+  ;; Order matters
+  {:background (Rect. 0 0 scale-w scale-h "black")
+   :s1         (Rect. 10 10 110 100 "rgb(200, 0, 0)")
+   :s2         (Rect. 60 60 110 100 "rgba(0,0,200,0.5)")
+   :paddle-1   (Paddle. 20 (- scale-h 120) 20 100 "dodgerblue")
+   :paddle-2   (Paddle. (- scale-w 40) 20 20 100 "dodgerblue")
+   :border     (Frame. 0 0 scale-w scale-h "dodgerblue" 2)})
+
+;;-----------------------------------------------------------------------------
+
+(defn draw-phase!
   [ctx]
-  (let [c (by-id "canvas")]
-    (-> ctx
-        (ctx-apply "clearRect" 0 0 scale-w scale-h)
-        (ctx-prop! "fillStyle" "green")
-        (ctx-apply "fillRect" 0 0 scale-w scale-h)
-        (ctx-prop! "lineWidth" "0.5")
-        (ctx-prop! "strokeStyle" "black")
-        (ctx-prop! "fillStyle" "rgb(200, 0, 0)")
-        (ctx-apply "strokeRect" 10 10 110 100)
-        (ctx-apply "fillRect" 10 10 110 100)
-        (ctx-prop! "fillStyle" "rgba(0,0,200,0.5)")
-        (ctx-apply "strokeRect" 60 60 110 100)
-        (ctx-apply "fillRect" 60 60 110 100)
-        ;;
-        ;; See if the virtual bounds are what I think they are.
-        (ctx-prop! "lineWidth" "4")
-        (ctx-prop! "strokeStyle" "dodgerblue")
-        (ctx-apply "strokeRect" 0 0 scale-w scale-h))))
+  (.clearRect ctx 0 0 scale-w scale-h)
+  (doseq [o (vals objects)]
+    (when (satisfies? IDrawable o)
+      (draw! o ctx))))
 
 (defn resize!
   [e]
@@ -72,14 +118,14 @@
         (attr! "width" w)
         (attr! "height" h))
     (.scale ctx (/ w scale-w) (/ h scale-h))
-    (draw! ctx)))
+    (draw-phase! ctx)))
 
 (defn- main
   []
   (println "Welcome to the Canvas Scratch App.")
   (listen! js/window "resize" resize!)
-  (draw! ctx)
+  (draw-phase! ctx)
   (resize! nil)
-  (draw! ctx))
+  (draw-phase! ctx))
 
 (set! (.-onload js/window) main)
