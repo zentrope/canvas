7e656ed93f4600dc4ae98185a8e4caa33b8188ab
Keith Irwin <keith@zentrope.com>
2015-03-11 21:38:20 -0700

    Bump libs.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies
   [[org.clojure/clojure "1.7.0-alpha5"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-   [org.clojure/clojurescript "0.0-3053"]]
+   [org.clojure/clojurescript "0.0-3058"]]
 
   :clean-targets
   ^{:protect false}
